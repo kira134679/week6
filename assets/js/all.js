a90b32f5454d37e4a423d168942451f6e22a9041
kira134679 <kira134679@gmail.com>
2022-08-31 23:28:37 +0800

    Update 2022-08-31T15:28:33.218Z

---
+++ assets/js/all.js
@@ -0,0 +1,45 @@
+"use strict";
+
+$(function () {
+  console.log('Hello Bootstrap5');
+});
+$(function () {
+  var progLangSwiper = new Swiper(".progLangSwiper", {
+    slidesPerView: "auto",
+    spaceBetween: 8,
+    centeredSlides: true,
+    autoplay: {
+      delay: 5000
+    },
+    loop: true,
+    breakpoints: {
+      992: {
+        spaceBetween: 16
+      }
+    }
+  });
+});
+$(function () {
+  var classesSwiper = new Swiper(".classesSwiper", {
+    slidesPerView: "auto",
+    spaceBetween: 16,
+    centeredSlides: false
+  });
+});
+$(function () {
+  var commentSwiper = new Swiper(".commentSwiper", {
+    effect: 'fade',
+    loop: true,
+    fadeEffect: {
+      crossFade: true
+    },
+    pagination: {
+      el: ".swiper-pagination"
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev'
+    }
+  });
+});
+//# sourceMappingURL=all.js.map
